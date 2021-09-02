a127f01da895b7ad67575ca2567d1b8210e3f373
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2021-09-02 22:37:56 +0900

    editing DistilKoBERT epoch&early_stopping_patience and

--- data_loader.py
+++ data_loader.py
@@ -8,20 +8,27 @@ from torchsampler import ImbalancedDatasetSampler
 
 def get_data_loaders(
     tokenizer: transformers.PreTrainedTokenizer,
-    batch_size: int = 64,
+    batch_size: int = 32,
     return_loader: bool = True,
     use_imbalanced: bool = True,
+    device="cpu",
 ):
     r"""토크나이저를 입력하면 해당 토크나이저로 인코딩 된 DataLoader를 반환하는 함수
     args:
         tokenizer: 사용할 토크나이저
-        batch_size: int: 배치 사이즈, 기본값=64,
+        batch_size: int: 배치 사이즈, 기본값=32,
         return_loader: bool: True면 DataLoader를 반환하고, False면 Dataset을 반환합니다.
         use_imbalanced: bool: ImbalancedDatasetSampler를 사용할 지의 여부, 기본값=True
-
     return:
         train_loader, val_loader, test_loader: Tuple[DataLoader]
         """
+
+    if use_imbalanced and device == "cuda":
+        raise ValueError(
+            "use_imbalanced == True에 device == 'cuda'이면 오류가 발생합니다. use_imbalanced를 False로 하거나 device를 cpu로 하십시오."
+        )
+
+    device = torch.device(device)
 
     def encode(data):
         return tokenizer(data["content"], padding="max_length", truncation=True)
@@ -39,6 +46,7 @@ def get_data_loaders(
     all_data.set_format(
         type="torch",
         columns=["input_ids", "token_type_ids", "attention_mask", "labels"],
+        device=device,
     )
 
     if not return_loader:
@@ -56,7 +64,7 @@ def get_data_loaders(
             all_data["train"], callback_get_label=get_label,
         )
         train_loader = DataLoader(
-            all_data["train"], batch_size=batch_size, shuffle=True, sampler=im_sampler
+            all_data["train"], batch_size=batch_size, sampler=im_sampler
         )
     else:
         train_loader = DataLoader(
@@ -67,4 +75,3 @@ def get_data_loaders(
     test_loader = DataLoader(all_data["test"], batch_size=batch_size)
 
     return train_loader, val_loader, test_loader
-
