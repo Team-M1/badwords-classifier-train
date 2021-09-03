9927f86ebc0456f50b8a3259af06c6f955869fe2
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2021-09-03 18:02:37 +0900

    uploading model

--- load_distilkobert_base.py
+++ load_distilkobert_base.py
@@ -1,5 +1,5 @@
 from transformers import DistilBertForSequenceClassification
 from tokenization_kobert import KoBertTokenizer
 
-model = DistilBertForSequenceClassification.from_pretrained("model/distilkobert")
+model = DistilBertForSequenceClassification.from_pretrained("DistilBERT/distilkobert")
 tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert", model_max_length=512)
